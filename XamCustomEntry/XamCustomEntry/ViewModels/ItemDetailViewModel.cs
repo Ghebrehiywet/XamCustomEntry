a0b56d2c54a4ba4df864be5909a7c3e963cbeeb8
Ghebrehiywet Fissaha <g.hiwetmit@gmail.com>
2019-05-08 15:38:22 +0300

    Add project files.

---
+++ XamCustomEntry/XamCustomEntry/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamCustomEntry.Models;
+
+namespace XamCustomEntry.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
